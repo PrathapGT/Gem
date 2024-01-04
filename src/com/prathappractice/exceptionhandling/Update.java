2f6a97d9c01e612d055941954b744549a1df547c
prathap <prathaprn29@gmail.com>
2024-01-04 11:10:55 +0530

    Practice program updated

---
+++ src/com/prathappractice/exceptionhandling/Update.java
@@ -0,0 +1,15 @@
+package com.prathappractice.exceptionhandling;
+
+public class Update {
+
+	public static void main(String[] args) {
+
+		
+		String s="PrathapReddy";
+		
+		System.out.println(s.length());
+		
+		System.out.println(s.toUpperCase());
+	}
+
+}
