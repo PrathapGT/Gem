e86e8bf13dade61ad7c32f93998053d9f12a724d
prathap <prathaprn29@gmail.com>
2024-01-05 12:58:57 +0530

    commit branch 1

---
+++ src/com/prathappractice/exceptionhandling/Changes.java
@@ -0,0 +1,18 @@
+package com.prathappractice.exceptionhandling;
+
+public class Changes {
+
+	public static void main(String[] args) {
+
+		
+		int i=10;
+		int j=20;
+		if(i>j) {
+			
+			System.out.println("I is bigger");
+		}else
+			System.out.println("J is bigger");
+			
+	}
+
+}
